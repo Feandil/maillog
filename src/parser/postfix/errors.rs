0bb6fd119e181dfda85e7822bf9bbc62b6f40e1d
Vincent Brillault <git@lerya.net>
2015-06-04 08:19:29 +0200

    Because it feels good to be useless

--- src/parser/postfix/errors.rs
+++ src/parser/postfix/errors.rs
@@ -21,7 +21,7 @@ impl fmt::Display for ParseError {
 			&ParseError::NonEndingQueue => "Non Ending Queue",
 			&ParseError::NonEndingProcess => "Non Ending Process",
 			&ParseError::BadProcessID => "Bad Process ID",
-			&ParseError::NonEndingQueueID =>"Non Ending Queue ID",
+			&ParseError::NonEndingQueueID => "Non Ending Queue ID",
 			&ParseError::PickupBadUID => "Pickup Bad UID",
 			&ParseError::PickupBadFrom => "Pickup Bad From",
 		};
@@ -32,5 +32,29 @@ impl fmt::Display for ParseError {
 impl fmt::Debug for ParseError {
 	fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
 		fmt::Display::fmt(self, fmt)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use std::fmt;
+
+	fn assert_print_eq(error: ParseError, expected: &'static str) {
+		assert_eq!(fmt::format(format_args!("{}", error)), expected);
+		assert_eq!(fmt::format(format_args!("{:?}", error)), expected);
+	}
+
+	#[test]
+	fn formatting() {
+		assert_print_eq(ParseError::DateTooShort, "Date Too Short");
+		assert_print_eq(ParseError::NonEndingHost, "Non Ending Host");
+		assert_print_eq(ParseError::MissingProcess, "Missing Process");
+		assert_print_eq(ParseError::NonEndingQueue, "Non Ending Queue");
+		assert_print_eq(ParseError::NonEndingProcess, "Non Ending Process");
+		assert_print_eq(ParseError::BadProcessID, "Bad Process ID");
+		assert_print_eq(ParseError::NonEndingQueueID, "Non Ending Queue ID");
+		assert_print_eq(ParseError::PickupBadUID, "Pickup Bad UID");
+		assert_print_eq(ParseError::PickupBadFrom, "Pickup Bad From");
 	}
 }
