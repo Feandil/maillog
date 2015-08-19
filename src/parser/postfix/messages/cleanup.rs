44e270fc0215c735cfeb96260ad0dd3810f2d2da
Vincent Brillault <git@lerya.net>
2015-08-19 10:05:56 +0200

    Add support for resent-message-id

--- src/parser/postfix/messages/cleanup.rs
+++ src/parser/postfix/messages/cleanup.rs
@@ -9,6 +9,7 @@ pub struct Cleanup {
 	inner: Inner,
 	message_id_s: usize,
 	message_id_e: usize,
+	resent: bool,
 }
 
 impl Deref for Cleanup {
@@ -26,13 +27,18 @@ impl Cleanup {
 }
 impl MessageParser for Cleanup {
 	fn parse(inner: Inner, start: usize) -> Result<Option<Message>, ParseError> {
-		let (message_id_s, message_id_e) = {
+		let (message_id_s, message_id_e, resent) = {
 			let rest = &inner.raw[start..];
-			if  !rest.starts_with(" message-id=") {
-				return Err(ParseError::CleanupNoMessageID);
-			}
-			let rest = &rest[12..];
-			let mut message_id_s = start + 12;
+			let (rest, message_id_s, resent) = {
+				if rest.starts_with(" message-id=") {
+					(&rest[12..], start + 12, false)
+				} else if rest.starts_with(" resent-message-id=") {
+					(&rest[19..], start + 19, true)
+				} else {
+					return Err(ParseError::CleanupNoMessageID);
+				}
+			};
+			let mut message_id_s = message_id_s;
 			let mut message_id_e = rest.len();
 			if &rest[message_id_e-1..message_id_e] == "\n" {
 				message_id_e = message_id_e - 1;
@@ -46,9 +52,9 @@ impl MessageParser for Cleanup {
 					message_id_s = message_id_s + 1;
 				}
 			}
-			(message_id_s, message_id_e)
+			(message_id_s, message_id_e, resent)
 		};
-		Ok(Some(Message::Cleanup { m: Cleanup { inner: inner, message_id_s: message_id_s, message_id_e:message_id_e } }))
+		Ok(Some(Message::Cleanup { m: Cleanup { inner: inner, message_id_s: message_id_s, message_id_e: message_id_e, resent: resent } }))
 	}
 }
 
@@ -94,6 +100,7 @@ mod tests {
 			Ok(Some(x)) => panic!("Wrong message parsed: {:?}", x)
 		};
 		assert_eq!(cleanup.message_id(), "20150803220001.5E2AA52093C@mail2.les-moocs-gmf.fr");
+		assert_eq!(cleanup.resent, false);
 		let s = "Aug  4 00:00:01 yuuai postfix-in/cleanup[22502]: A071220883: message-id=20150803220001.5E2AA52093C@mail2.les-moocs-gmf.fr".to_string();
 		let cleanup = match parse_cleanup(s) {
 			Err(x) => panic!("Parser Error: {}", x),
@@ -102,6 +109,16 @@ mod tests {
 			Ok(Some(x)) => panic!("Wrong message parsed: {:?}", x)
 		};
 		assert_eq!(cleanup.message_id(), "20150803220001.5E2AA52093C@mail2.les-moocs-gmf.fr");
+		assert_eq!(cleanup.resent, false);
+		let s = "Aug  4 00:03:09 yuuai postfix-in/cleanup[22656]: 40A67208A3: resent-message-id=<PbhLmifNtVG.A.mh.ZU-vVB@bendel>".to_string();
+		let cleanup = match parse_cleanup(s) {
+			Err(x) => panic!("Parser Error: {}", x),
+			Ok(None) => panic!("This should not have been ignored"),
+			Ok(Some(Message::Cleanup{m:x})) => x,
+			Ok(Some(x)) => panic!("Wrong message parsed: {:?}", x)
+		};
+		assert_eq!(cleanup.message_id(), "PbhLmifNtVG.A.mh.ZU-vVB@bendel");
+		assert_eq!(cleanup.resent, true);
 		let s = "Aug  4 00:00:01 yuuai postfix-in/cleanup[22502]: A071220883: message-id=<20150803220001.5E2AA52093C@mail2.les-moocs-gmf.fr>".to_string();
 		let cleanup = match parse_cleanup(s) {
 			Err(x) => panic!("Parser Error: {}", x),
@@ -110,6 +127,7 @@ mod tests {
 			Ok(Some(x)) => panic!("Wrong message parsed: {:?}", x)
 		};
 		assert_eq!(cleanup.message_id(), "20150803220001.5E2AA52093C@mail2.les-moocs-gmf.fr");
-		assert_eq!(fmt::format(format_args!("{:?}", cleanup)), "Cleanup { inner: Inner { raw: \"Aug  4 00:00:01 yuuai postfix-in/cleanup[22502]: A071220883: message-id=<20150803220001.5E2AA52093C@mail2.les-moocs-gmf.fr>\", host_e: 21, queue_s: 22, queue_e: 32, process: Cleanup, pid: 22502, queue_id_s: 49, queue_id_e: 59 }, message_id_s: 73, message_id_e: 122 }");
+		assert_eq!(cleanup.resent, false);
+		assert_eq!(fmt::format(format_args!("{:?}", cleanup)), "Cleanup { inner: Inner { raw: \"Aug  4 00:00:01 yuuai postfix-in/cleanup[22502]: A071220883: message-id=<20150803220001.5E2AA52093C@mail2.les-moocs-gmf.fr>\", host_e: 21, queue_s: 22, queue_e: 32, process: Cleanup, pid: 22502, queue_id_s: 49, queue_id_e: 59 }, message_id_s: 73, message_id_e: 122, resent: false }");
 	}
 }
