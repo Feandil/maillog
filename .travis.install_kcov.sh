49c657e4b6f5b42c43e0b456a6f2bdb7a949f2cd
Vincent Brillault <git@lerya.net>
2015-08-19 11:52:10 +0200

    travis: cache kcov

--- .travis.install_kcov.sh
+++ .travis.install_kcov.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
 set -e
-wget https://github.com/Feandil/kcov/archive/master.tar.gz
-tar xzf master.tar.gz
-mkdir kcov-master/build
-cd kcov-master/build
+
+# Using cached value
+if test !-d $HOME/kcov
+then
+  mkdir $HOME/kcov
+fi
+cd $HOME/kcov
+
+if test -d .git
+then
+  git pull -f
+else
+  git clone https://github.com/Feandil/kcov.git .
+  mkdir build
+fi
+
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=$HOME ..
 make
 make install
-cd ../..
 
